de3396e0063281815f00168477c843a9dc2d7748
monojitwithu <monojitwithu@gmail.com>
2021-07-05 18:45:32 +0530

    Back Button Added

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,8 +1,17 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
 
 const  Header=()=>{
+    const history=useHistory()
+
+    const backToProductPage=()=>{
+        history.push("/")
+
+    }
+
     return(
         <div className="header">
+            <span onClick={backToProductPage}>Back</span>
         <p>Product OverView</p>
         </div>
     )
